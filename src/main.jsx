49262306db4d14f822d829f82fffb12beff55c0e
AndrewHypervenom <pachonandres721@gmail.com>
2026-03-22 16:24:37 -0500

    feat: auto-actualización al detectar nueva versión del SW y no cachear index.html

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,19 @@ import './i18n/index.js'
 import './index.css'
 import App from './App.jsx'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(reg => {
+    reg.addEventListener('updatefound', () => {
+      const newWorker = reg.installing
+      newWorker.addEventListener('statechange', () => {
+        if (newWorker.state === 'activated' && navigator.serviceWorker.controller) {
+          window.location.reload()
+        }
+      })
+    })
+  })
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
